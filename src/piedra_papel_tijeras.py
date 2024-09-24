5a18ee600eef97d1d208efa266ec9c8962588753
salviiicv0 <salcarvaz@alum.us.es>
2024-09-24 18:50:17 +0200

    apartadoD

--- src/piedra_papel_tijeras.py
+++ src/piedra_papel_tijeras.py
@@ -14,3 +14,15 @@ def usuario_decide_jugada():
     '''
     eleccion_usuario = input("Elige piedra, papel o tijeras: ")    
     return eleccion_usuario
+
+def determina_ganador(jugada_usuario, jugada_ordenador):
+    if jugada_usuario == jugada_ordenador:
+        return "Empate"
+    elif jugada_usuario == "piedra" and jugada_ordenador == "tijeras":
+        return "Ganaste"
+    elif jugada_usuario == "tijeras" and jugada_ordenador == "papel":
+        return "Ganaste"
+    elif  jugada_usuario == "papel" and jugada_ordenador == "piedra":
+        return "Ganaste"
+    else:
+        return "Perdiste"
--- src/piedra_papel_tijeras_test.py
+++ src/piedra_papel_tijeras_test.py
@@ -18,7 +18,26 @@ def test_usuario_decide_jugada():
     print("El usuario eligió:", eleccion)
     print()
 
+def test_determina_ganador(eleccion_jugador, eleccion_ordenador):
+    '''
+    Test para la función determina_ganador.
+    '''
+    print("Testeando determina_ganador...")        
+    print(f"Jugador: {eleccion_jugador} vs. Ordenador: {eleccion_ordenador}")
+    resultado = determina_ganador(eleccion_jugador, eleccion_ordenador)
+    print("Resultado:", resultado)
+    print()
+
 # Función principal
 if __name__ == "__main__":
     #test_ordenador_decide_jugada()
-    test_usuario_decide_jugada()
+    #test_usuario_decide_jugada()
+    test_determina_ganador("piedra", "tijeras")
+    test_determina_ganador("piedra", "papel")
+    test_determina_ganador("piedra", "piedra")
+    test_determina_ganador("tijeras", "tijeras")
+    test_determina_ganador("tijeras", "papel")
+    test_determina_ganador("tijeras", "piedra")
+    test_determina_ganador("papel", "tijeras")
+    test_determina_ganador("papel", "papel")
+    test_determina_ganador("papel", "piedra")
